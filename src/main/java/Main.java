32235b9b2c368ee0eb37bc5f1d7d1697a7ada8a6
parlog.valentin1 <parlog.valentin@gmail.com>
2022-06-17 18:23:12 +0300

    I have created the Observer

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import classes.EmailTopic;
+import interfaces.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        //EmailTopic emailTopic = new EmailTopic();
+    }
+}
